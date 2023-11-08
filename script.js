caecaa449fcd6dfa6ffa0f6e85ab1f4c0dc2aa9f
quin <queenshafaraniabilqis@gmail.com>
2023-11-08 09:49:54 +0700

    js

---
+++ script.js
@@ -0,0 +1,15 @@
+const quotes = [
+  "Every journey has its final day. Don't rush!",
+  "I need your actions, not your words.",
+  "Everyone has their own perspective, there's nothing strange about that.",
+  "Reality is the stillness buried deep beneath the illusion.",
+  "Inactivity serves no purpose whatsoever.",
+  "I am Qiqi. I am a zombie. And I forgot what comes next.",
+  "Going out into the world and investigating, turning the 'unknown' into the 'known.'",
+];
+
+let words = [];
+let wordIndex = 0;
+// Starting Time
+let startTime = Date.now();
+// page elements
